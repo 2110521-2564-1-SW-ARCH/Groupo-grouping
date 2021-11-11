c737ab003408794f0ecb363ac3fb7253f9e4a655
wongtawan-j <work@wongtawan.dev>
2021-11-11 22:28:12 +0700

    add healthcheck

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,6 +3,9 @@ import BoardRouter from "./board.router";
 
 const routes = Router();
 
+routes.get('/', (req, res) => {
+    res.status(200).json({});
+});
 routes.use('/board', BoardRouter);
 
 export default routes;
